61857aa54420f54b2af8a6fd6a7362efbfb514c1
Adam Babik <a.babik@designfortress.com>
2018-10-10 15:52:41 +0200

    pubchats: add shh_unique_authors_total metric (#4)

--- cmd/pubchats/metrics.go
+++ cmd/pubchats/metrics.go
@@ -14,10 +14,16 @@ var (
 		Name:      "messages_total",
 		Help:      "Received messages counter.",
 	}, []string{"chat"})
+	uniqueCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "shh",
+		Name:      "unique_authors_total",
+		Help:      "Unique authoers of the messages.",
+	}, []string{"chat"})
 )
 
 func init() {
 	prometheus.MustRegister(messagesCounter)
+	prometheus.MustRegister(uniqueCounter)
 }
 
 func startMetricsServer(addr string) {
