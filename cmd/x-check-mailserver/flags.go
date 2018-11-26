a290aeaa1a48278f4292a7262ff934ac0bc4f4ae
Adam Babik <a.babik@designfortress.com>
2018-11-26 13:28:40 +0100

    add x-check-mailserver (#7)

---
+++ cmd/x-check-mailserver/flags.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+
+	"github.com/spf13/pflag"
+	"github.com/status-im/status-go/params"
+)
+
+var (
+	fleet       = pflag.StringP("fleet", "f", params.FleetBeta, "cluster fleet")
+	mailservers = pflag.StringArrayP("mailservers", "m", nil, "a list of mail servers")
+	duration    = pflag.DurationP("duration", "l", time.Hour*24, "length of time span from now")
+	channels    = pflag.StringArrayP("channel", "p", []string{"status"}, "name of the channel")
+	verbosity   = pflag.StringP("verbosity", "v", "INFO", "verbosity level of status-go, options: crit, error, warning, info, debug")
+)
+
+func init() {
+	pflag.Parse()
+}
